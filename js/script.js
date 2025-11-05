f48bc62e5df5118568c3871bb826acd88d82eb8d
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-04 20:56:41 -0800

    done exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 // YOUR CODE HERE
+
+const redButton = document.querySelector("#btn1");
+const redOutput = document.querySelector("#output1");
+
+redButton.addEventListener("click", () => {
+  redOutput.style.color = "red";
+});
+
+const helloButton = document.querySelector("#btn2");
+const helloOutput = document.querySelector("#output2");
+
+helloButton.addEventListener("click", () => {
+  const newText = document.createElement("span");
+  newText.textContent = "Hello world";
+  helloOutput.append(newText);
+});
+
+const deleteButton = document.querySelector("#btn3");
+const deleteOutputParagraph = document.querySelector("#output3 p");
+
+deleteButton.addEventListener("click", () => {
+  deleteOutputParagraph.remove();
+});
+
+const bothChangeButton = document.querySelector("#btn4");
+const bothChangeOutput = document.querySelectorAll("#output4 p");
+
+bothChangeButton.addEventListener("click", () => {
+  bothChangeOutput.forEach((element) => {
+    element.style.color = "red";
+  });
+});
+
+const inputButton = document.querySelector("#btn5");
+
+inputButton.addEventListener("click", () => {
+  const input = document.querySelector("#message");
+  console.log(input.value);
+});
